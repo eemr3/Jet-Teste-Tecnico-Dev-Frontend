9f722073ca7f66e8a2d57bcf3b7ecb430130476d
Emerson Moreira <eemr3@yahoo.com.br>
2024-02-28 23:16:29 -0300

    feat: cria component CardList e adiciona component CarCard

---
+++ src/components/cards/CardList.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { IDataCar } from '../../common/interfaces';
+import { CarCard } from './Card';
+
+export const CardList = ({ cars }: IDataCar) => {
+  return (
+    <div className="flex flex-wrap -mx-4">
+      {cars.map((item, index) => (
+        <CarCard key={index} item={item} />
+      ))}
+    </div>
+  );
+};
