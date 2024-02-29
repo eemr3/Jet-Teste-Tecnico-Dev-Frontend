8adac4b58063e84fdc91ee527201e7e8fe50c772
Emerson Moreira <eemr3@yahoo.com.br>
2024-02-28 23:09:25 -0300

    feat: cria component ActionCell e adiciona o component MenuOverflow

---
+++ src/components/table/ActionCell.tsx
@@ -0,0 +1,11 @@
+'use client';
+import MenuOverflow from '../menu/MenuOverflow';
+import { TableCell } from './TableCell';
+
+export const ActionCell = () => {
+  return (
+    <TableCell>
+      <MenuOverflow />
+    </TableCell>
+  );
+};
